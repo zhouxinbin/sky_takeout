771e42a39001605ecc457d51bb2adf87edf0324b
zxb <chousinbin@gmail.com>
2026-07-11 03:18:02 +0800

    登陆密码 MD5 验证

--- back_end/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ back_end/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -38,9 +38,8 @@ public class EmployeeServiceImpl implements EmployeeService {
             throw new AccountNotFoundException(MessageConstant.ACCOUNT_NOT_FOUND);
         }
 
-        //密码比对
-        // TODO 后期需要进行md5加密，然后再进行比对
-        if (!password.equals(employee.getPassword())) {
+        //密码比对 MD5 加密处理
+        if (!DigestUtils.md5DigestAsHex(password.getBytes()).equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
         }
