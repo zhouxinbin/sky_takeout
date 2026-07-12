aef5d0eff1b99eeaca6f3c4922293821694499a0
zxb <chousinbin@gmail.com>
2026-07-12 21:10:28 +0800

    实现员工分页查询功能

--- back_end/sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ back_end/sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -16,9 +18,23 @@ public interface EmployeeMapper {
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 
-@Insert("insert into employee " +
+    /**
+     * 新增员工
+     * @param employee
+     */
+    @Insert("insert into employee " +
         "(name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user) " +
         "values " +
         "(#{name}, #{username}, #{password}, #{phone}, #{sex}, #{idNumber}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
     void insert(Employee employee);
+
+    /**
+     * 员工分页查询
+     *
+     * @param employeePageQueryDTO
+     * @return
+     */
+    // IDEA 安装 MybatisX 插件之后 option + enter 快捷键 生成 SQL 语句到 EmployeeMapper.XML 文件
+    // 调用 EmployeeMapper.xml 文件中的 pageQuery SQL 语句
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
