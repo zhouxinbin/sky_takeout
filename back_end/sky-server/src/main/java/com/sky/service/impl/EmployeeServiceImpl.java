8acbce76a553a11513345a1f596f906f74b085e2
zxb <chousinbin@gmail.com>
2026-07-12 17:04:42 +0800

    实现管理员新增功能

--- back_end/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ back_end/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,9 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,9 +11,12 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -52,5 +57,29 @@ public class EmployeeServiceImpl implements EmployeeService {
         //3、返回实体对象
         return employee;
     }
+
+    /**
+     * 新增员工
+     * @param employeeDTO
+     */
+    @Override
+    public void save(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+        // 属性拷贝
+        BeanUtils.copyProperties(employeeDTO, employee);
+        // 状态常量类
+        employee.setStatus(StatusConstant.ENABLE);
+        // 密码常量类 默认密码 MD5 加密
+        employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+        // TODO 动态获取ID
+        employee.setCreateUser(10L);
+        employee.setUpdateUser(10L);
+
+        // 调用插入
+        employeeMapper.insert(employee);
+    }
+
 
 }
