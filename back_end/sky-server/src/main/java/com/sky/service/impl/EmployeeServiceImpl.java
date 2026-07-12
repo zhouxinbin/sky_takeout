c6144d913e24cb09ed7ee0a4f65cff6aec38704e
zxb <chousinbin@gmail.com>
2026-07-12 17:26:14 +0800

    JWT 动态获取管理员 ID

--- back_end/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ back_end/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.sky.service.impl;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
@@ -73,9 +74,9 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
         employee.setCreateTime(LocalDateTime.now());
         employee.setUpdateTime(LocalDateTime.now());
-        // TODO 动态获取ID
-        employee.setCreateUser(10L);
-        employee.setUpdateUser(10L);
+        // 动态获取 ID
+        employee.setCreateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId());
 
         // 调用插入
         employeeMapper.insert(employee);
